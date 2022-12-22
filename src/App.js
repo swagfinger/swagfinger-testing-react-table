497e19d9ed5a1ad392283d4ed8f68233d206e3b4
swagfinger <swagfinger@gmail.com>
2022-12-22 13:10:07 +0800

    SORTING

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import './App.css';
-import BasicTable from './components/BasicTable';
+import SortingTable from './components/SortingTable';
 
 function App() {
   return (
     <div className="App">
-      <BasicTable/>
+      <SortingTable/>
     </div>
   );
 }
