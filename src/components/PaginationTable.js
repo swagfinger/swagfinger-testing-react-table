32a6dc43a06f9efbe047de30408e49d32d70d19f
swagfinger <swagfinger@gmail.com>
2022-12-22 15:12:34 +0800

    Pagination - goto Page

--- src/components/PaginationTable.js
+++ src/components/PaginationTable.js
@@ -11,9 +11,12 @@ const PaginationTable = () => {
   const columns = useMemo(()=> COLUMNS, []);
   const data = useMemo(()=> MOCK_DATA, []);
 
-  const {getTableProps, getTableBodyProps, headerGroups, footerGroups, page, prepareRow, nextPage, previousPage, canNextPage, canPreviousPage, pageOptions, state} = useTable({
+  const {getTableProps, getTableBodyProps, headerGroups, footerGroups, page, prepareRow, nextPage, previousPage, canNextPage, canPreviousPage, pageOptions, state, gotoPage, pageCount} = useTable({
     columns,
-    data
+    data,
+    initialState:{
+      pageIndex: 3 //zero indexed
+    }
   }, usePagination);
 
   const {pageIndex} = state;
@@ -59,8 +62,19 @@ const PaginationTable = () => {
     </table>
     <div>
       <span>Page <strong>{pageIndex + 1} of {pageOptions.length}</strong></span>
+      <span>| goto page:<input type="number" defaultValue={pageIndex+1} 
+      
+      onChange={e=> {
+        const pageNumber = e.target.value ? Number(e.target.value) -1 : 0;
+        gotoPage(pageNumber) 
+      }} 
+      
+      style={{width: '50px'}}
+      /></span>
+      <button onClick={()=> gotoPage(0)} disabled={!canPreviousPage}>{'<<'}</button>
       <button onClick={()=> previousPage()} disabled={!canPreviousPage}>previous</button>
       <button onClick={()=> nextPage()} disabled={!canNextPage}>next</button>
+      <button onClick={()=> gotoPage(pageCount-1)} disabled={!canNextPage}>{'>>'}</button>
     </div>
     </>
   );
